6979d84adecab0f8f70644c7cea7a4d9dcdd1e43
“[MoniqueFunch]” <[moni5968@stud.kea.dk]>
2021-10-04 09:02:27 +0200

    splash semidone

---
+++ script.js
@@ -0,0 +1,11 @@
+// window.addEventListener(
+//   "scroll",
+//   function () {
+//     if (window.scrollY > 50) {
+//       $(".navbar").fadeOut();
+//     } else {
+//       $(".navbar").fadeIn();
+//     }
+//   },
+//   false
+// );
