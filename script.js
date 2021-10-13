b2b713c3eb9f7e574bb7b129fa94dbeddbd0f919
“[MoniqueFunch]” <[moni5968@stud.kea.dk]>
2021-10-13 16:09:24 +0200

    Aflevering 1.0

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-// window.addEventListener(
-//   "scroll",
-//   function () {
-//     if (window.scrollY > 50) {
-//       $(".navbar").fadeOut();
-//     } else {
-//       $(".navbar").fadeIn();
-//     }
-//   },
-//   false
-// );
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+  //   const img = section.querySelectorAll("img");
+  const p = section.querySelectorAll("p");
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: "200% 200%",
+        end: "+=40",
+        scrub: true,
+        // pin: true,
+        // markers: true,
+      },
+    })
+    .from(p, {
+      duration: 1,
+      opacity: 0,
+    });
+  // .from(p, {
+  //   y: 100,
+  //   duration: 1,
+  //   opacity: 0,
+  //   stagger: 1,
+  // });
+});
